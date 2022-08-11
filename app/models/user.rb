6b947a17ea11532386c29b06469410b441311017
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-08-11 22:27:13 +0200

    Add validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   def three_most_recent_posts
     posts.order(created_at: :desc).first(3)
   end
+
+  validates :name, presence: true, length: { in: 2..25 }
+  validates :post_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
