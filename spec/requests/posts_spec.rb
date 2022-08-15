3b179724a4ef224c506d6c570f712f06bad0c25a
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-08-16 00:41:45 +0200

    Add posts for users controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  context 'When testing GET #index' do
+    before(:example) { get '/users/:id/posts' }
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
+    end
+
+    it 'should display correct placeholder' do
+      expect(response.body).to include('Here is a list of posts for a given user')
+    end
+  end
+
+  context 'When testing GET #show' do
+    before(:example) { get '/users/:id/posts/:id' }
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'index' template" do
+      expect(response).to render_template('show')
+    end
+
+    it 'should display correct placeholder' do
+      expect(response.body).to include('Here is a specific post by id')
+    end
   end
 end
