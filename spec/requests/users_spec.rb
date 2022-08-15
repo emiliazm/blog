ddc6c1bf1c3b4bab0304d5c26f8d26bb1d96884b
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-08-16 00:41:32 +0200

    Add tests for users controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  context 'When testing GET #index' do
+    before(:example) { get '/users' }
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
+    end
+
+    it 'should display correct placeholder' do
+      expect(response.body).to include('Here is a list of users order by id')
+    end
+  end
+
+  context 'When testing GET #show' do
+    before(:example) { get '/users/:id' }
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'index' template" do
+      expect(response).to render_template('show')
+    end
+
+    it 'should display correct placeholder' do
+      expect(response.body).to include('Here is the information for a specific user')
+    end
   end
 end
