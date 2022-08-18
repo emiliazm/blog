415efe71a71c94c341df94c2b490e1737e2b4960
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-08-19 00:54:39 +0200

    Add new and create for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,33 @@ class PostsController < ApplicationController
     @posts = @user.posts.order(id: :desc)
   end
 
+  def new
+    @post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: @post } }
+    end
+  end
+
+  def create
+    @post = current_user.posts.create(post_params)
+
+    if @post.save
+      flash[:success] = 'Post created succesfully'
+      redirect_to user_posts_path(current_user.id)
+    else
+      flash[:error] = 'It occur an error when creating the post'
+      render :new
+    end
+  end
+
   def show
     @post = Post.find_by(id: params[:id])
     @user = User.find_by(id: params[:user_id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
